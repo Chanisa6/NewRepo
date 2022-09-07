e076433660453fc9ba6fdd82ea99e47c06f0b8f0
Chanisa Visser <s1183213@student.windesheim.nl>
2022-09-07 13:16:23 +0200

    first lesson program

---
+++ week1opdracht1/week1opdracht1/main.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+int main(void) {
+	int a;
+	int b;
+	int c;
+
+	a = 10;
+
+	printf("Voer een geheel getal in..");
+	scanf_s("%d", &b);
+
+	c = (a * b) * b / a;
+
+		if (b > 9) {
+
+			printf("Het getal groter was groter dan 9\n");
+	}
+		else {
+			printf("Het groter ");
+		}
+
+		a = 10;
+		b = 20;
+
+		printf("Programeren is leuk! %d %d \n",a,b);
+		printf("A keer B is %d", c);
+
+
+
+		return 0;
+
+}
+
+
+#include <stdio.h>
+
+scanf("&d", % value);
