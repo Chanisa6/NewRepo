55829c04fb031212f2bf086e59b5c2fac30afbdc
Chanisa Visser <s1183213@student.windesheim.nl>
2022-09-12 17:55:00 +0200

    opdracht 2 van week 1

--- week1opdracht1/week1opdracht1/main.cpp
+++ week1opdracht1/week1opdracht1/main.cpp
@@ -1,39 +1,50 @@
-#include <stdio.h>
+#include <stdio.h> 
+
+// v = final velocity
+// s = distance traversed
+// a = acceleration of an object
+// t = the time that has elapsed
+// u = the initial velocity
+
+int main(void)
+{
+	int velocity; // u
+	int acceleration; // a
+	int time; // t
+	int finalVelocity; // v
+	int distance; // s
+
+	 printf("Enter the initial velocity\n");
+	 scanf("%d", &velocity);
+
+	 printf("Enter the acceleration of an object\n");
+	 scanf("%d", &acceleration);
+
+	 printf("Enter the time that has elapsed\n");
+	 scanf("%d", &time);
+	 
+	 
+
+	 finalVelocity = velocity + (acceleration * time);
 
 
-int main(void) {
-	int a;
-	int b;
-	int c;
+	 printf("%d", finalVelocity);
+	 printf("\n");
 
-	a = 10;
+	 printf("Enter the initial velocity\n");
+	 scanf("%d", &velocity);
 
-	printf("Voer een geheel getal in..");
-	scanf_s("%d", &b);
+	 printf("Enter the acceleration of an opject\n");
+	 scanf("%d", &acceleration);
 
-	c = (a * b) * b / a;
+	 
+	 distance = (velocity * time) + ((0,5 * acceleration) * (time * time));
 
-		if (b > 9) {
-
-			printf("Het getal groter was groter dan 9\n");
-	}
-		else {
-			printf("Het groter ");
-		}
-
-		a = 10;
-		b = 20;
-
-		printf("Programeren is leuk! %d %d \n",a,b);
-		printf("A keer B is %d", c);
-
-
-
-		return 0;
+	 printf("%d", distance);
 
 }
 
 
-#include <stdio.h>
 
-scanf("&d", % value);
+
+
